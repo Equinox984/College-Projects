dd21f94b089b3b3e054ec16a38741ee7faaa9f6d
Equinox984 <lopezkris984@proton.me>
2025-11-24 00:39:46 -0600

    Fixed crash when entering a string or 0.

--- Python_Projects/calculator.py
+++ Python_Projects/calculator.py
@@ -2,13 +2,16 @@
 
 print("Hello! Welcome to Equinox Calculator \n")
 
-n1 = (float(input("Write the first number: \n").strip()))
-n2 = (float(input("Write the second number: \n").strip()))
-
-add = n1 + n2
-sub = n1 - n2
-mul = n1 * n2
-div = n1 / n2
+try:
+    n1 = float(input("Write the first number: \n").strip())
+    n2 = float(input("Write the second number: \n").strip())
+    add = n1 + n2
+    sub = n1 - n2
+    mul = n1 * n2
+    div = n1 / n2
+except (ValueError, ZeroDivisionError):
+    print("Invalid input. Please enter a valid number.")
+    exit()
 
 print("\n")
 print("===== Results =====")
