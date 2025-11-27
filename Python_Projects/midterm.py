636f723b563d5c56cfc00c47fa852a9378bd87c1
Equinox984 <lopezkris984@proton.me>
2025-11-27 10:49:18 -0600

    Refactor user score input and feedback logic

--- Python_Projects/midterm.py
+++ Python_Projects/midterm.py
@@ -1,16 +1,23 @@
+"""Midterm Python Exercise"""
+
+# Welcome Message
 separator = "============================="
 print("\n", separator)
 print("Welcome to Equinox Grade Feedback Program!")
 print(separator, "\n")
 
 # Asking the User Score
-userScore = float(input("Please enter your grade for analysis: \n"))
+userScore = float(
+    input("Please enter your grade for analysis (between 0 - 100): \n"))
 
 # Results
 print("\nCalculating...\n")
-if userScore >= 90:
+
+if userScore > 100 or userScore < 0:
+    print("Impossible. Keep Dreaming")
+elif userScore >= 90 and userScore <= 100:
     print("Excellent. You are awesome!")
-elif userScore >= 70:
-    print("Good. Decent result.")
-else:
+elif userScore <= 70:
     print("Needs Improvement.")
+else:
+    print("Good. Decent Result")
