e8d81e902043a70988f9d19e7c9e698e90ac10c7
Equinox984 <lopezkris984@proton.me>
2025-12-08 20:15:57 -0600

    Change grade threshold condition from <= to <

--- Python_Projects/grade_calculator.py
+++ Python_Projects/grade_calculator.py
@@ -3,7 +3,7 @@
 grades = [85, 92, 78, 90, 65]
 
 for grade in grades:
-    if grade <= 70:
+    if grade < 70:
         print(f"Grade: {grade}: Needs Improvement")
     else:
         print(f"Grade: {grade}")
