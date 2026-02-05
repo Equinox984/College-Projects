aff5a89a7bf772ebb63708a1d02dc60251e447de
Equinox984 <lopezkris984@proton.me>
2026-02-05 10:57:55 -0600

    Enhance dictionary output and manage entries
    Added print formatting and deletion of countries from the dictionary.

--- Python_Projects/dictionary_2.py
+++ Python_Projects/dictionary_2.py
@@ -9,4 +9,18 @@ country_capitals["Nicaragua"] = "Managua"
 country_capitals["Ecuador"] = "Quito"
 country_capitals["Bahamas"] = "Nassau"
 
-print(country_capitals)
+print(country_capitals, "\n")
+
+# Delete Countries
+del country_capitals["Germany"]
+del country_capitals["Chile"]
+print(country_capitals, "\n")
+
+# Remove Everything
+# country_capitals.clear()
+# print(country_capitals)
+
+# Countries Listing
+for six_seven in country_capitals:
+    print(f"I want to visit -> {six_seven}")
+    print(f"And I want to go to the capital city -> {country_capitals[six_seven]}\n")
