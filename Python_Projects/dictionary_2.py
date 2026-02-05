d3e8c5274506a0565566a9778f7f0b6336734dbd
Equinox984 <lopezkris984@proton.me>
2026-02-05 10:43:49 -0600

    Add more country-capital pairs to dictionary
    Added additional country-capital pairs to the dictionary.

---
+++ Python_Projects/dictionary_2.py
@@ -0,0 +1,12 @@
+# Creating a Dictionary
+country_capitals = {"Germany": "Berlín", "Canada": "Ottawa", "England": "London"}
+
+country_capitals["Paraguay"] = "Asunción"
+country_capitals["Guyana"] = "Georgetown"
+country_capitals["Trinidad y Tobago"] = "Port of Spain"
+country_capitals["Chile"] = "Santiago"
+country_capitals["Nicaragua"] = "Managua"
+country_capitals["Ecuador"] = "Quito"
+country_capitals["Bahamas"] = "Nassau"
+
+print(country_capitals)
