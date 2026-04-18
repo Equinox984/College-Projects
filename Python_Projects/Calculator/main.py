52229421786d3a550a0078ce6648d4f2cc88268e
Equinox984 <lopezkris984@proton.me>
2026-04-17 21:23:59 -0600

    Add files via upload

---
+++ Python_Projects/Calculator/main.py
@@ -0,0 +1,56 @@
+from calculator.operations import Addition, Division, Multiplication, Subtraction
+from calculator.utils import get_number, print_result
+
+
+def main():
+    print("Welcome to the Modular Calculator!")
+
+    while True:
+        try:
+            choice = int(
+                input("""
+--- Welcome to Equinox Calculator ---
+[1] Addition
+[2] Subtraction
+[3] Multiplication
+[4] Division
+[5] Exit
+
+Enter your choice (1-5) -> """)
+            )
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            continue
+
+        if choice == 1:
+            a = get_number("Enter first number -> ")
+            b = get_number("Enter second number -> ")
+            op = Addition(a, b)
+            print_result("Addition", op.calculate())
+        elif choice == 2:
+            a = get_number("Enter first number -> ")
+            b = get_number("Enter second number -> ")
+            op = Subtraction(a, b)
+            print_result("Subtraction", op.calculate())
+        elif choice == 3:
+            a = get_number("Enter first number -> ")
+            b = get_number("Enter second number -> ")
+            op = Multiplication(a, b)
+            print_result("Multiplication", op.calculate())
+        elif choice == 4:
+            a = get_number("Enter first number -> ")
+            b = get_number("Enter second number -> ")
+            try:
+                op = Division(a, b)
+                print_result("Division", op.calculate())
+            except ZeroDivisionError:
+                print("\nERROR: Cannot divide by zero.")
+        elif choice == 5:
+            print("\nBye! See you next time.")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
